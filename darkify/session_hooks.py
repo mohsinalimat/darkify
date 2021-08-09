c3e5329188a087e61aacc70ccc3392ce13c92c5b
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-08-09 07:52:03 +0530

    chore: Add doc comment

--- darkify/session_hooks.py
+++ darkify/session_hooks.py
@@ -2,5 +2,6 @@ import frappe
 
 
 def make_it_dark():
+	"""Switch to dark theme for all users"""
 	for user in frappe.get_all("User", pluck="name"):
 		frappe.db.set_value("User", user, "desk_theme", "Dark")
