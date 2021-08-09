5311b104a9871bd4b34e95678980d83432dee571
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-08-09 07:55:20 +0530

    refactor: Use separate variable for user list

--- darkify/session_hooks.py
+++ darkify/session_hooks.py
@@ -3,5 +3,6 @@ import frappe
 
 def make_it_dark():
 	"""Switch to dark theme for all users"""
-	for user in frappe.get_all("User", pluck="name"):
+	all_users = frappe.get_all("User", pluck="name")
+	for user in all_users:
 		frappe.db.set_value("User", user, "desk_theme", "Dark")
