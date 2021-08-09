798382726672f84332205b1d4a1eeae66c3839ab
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-08-09 07:41:49 +0530

    fix: Remove print statement

--- darkify/session_hooks.py
+++ darkify/session_hooks.py
@@ -2,6 +2,5 @@ import frappe
 
 
 def make_it_dark():
-	print("Switching to dark mode for all users.")
 	for user in frappe.get_all("User", pluck="name"):
 		frappe.db.set_value("User", user, "desk_theme", "Dark")
